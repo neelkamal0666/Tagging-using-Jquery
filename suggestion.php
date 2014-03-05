d214216652d4afeec339585b136c16752cc66e70
Neel Kamal <neelkamal0666@gmail.com>
2014-03-05 12:45:16 +0530

    Create suggestion.php

---
+++ suggestion.php
@@ -0,0 +1,18 @@
+
+<table class="table table-bordered table-hover table-striped tablesorter">
+                <tbody>
+                  <tr>
+                    <th><a href="#" id="coding" class="tag_suggestion">Coding</a></th>
+                  </tr>
+                </tbody>
+				<tbody>
+                  <tr>
+                    <th><a href="#" id="web" class="tag_suggestion">web</a></th>
+                  </tr>
+                </tbody>
+				<tbody>
+                  <tr>
+                    <th><a href="#" id="study" class="tag_suggestion">study</a></th>
+                  </tr>
+                </tbody>
+</table>
